e03a6a1211d191fad30c2c840727bb98b8690a1c
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2013-04-25 06:58:35 +0900

    fix mt-safe problem.

--- empros-core/src/main/java/org/codelibs/empros/store/CsvStore.java
+++ empros-core/src/main/java/org/codelibs/empros/store/CsvStore.java
@@ -9,6 +9,8 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.regex.Pattern;
 
 import jp.sf.orangesignal.csv.CsvConfig;
@@ -17,6 +19,7 @@ import jp.sf.orangesignal.csv.CsvWriter;
 import org.apache.commons.io.IOUtils;
 import org.codelibs.empros.event.Event;
 import org.codelibs.empros.exception.EmprosDataStoreException;
+import org.seasar.util.message.MessageFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -45,9 +48,11 @@ public class CsvStore implements DataStore {
 
     protected String outputEncoding = "UTF-8";
 
-    protected volatile String currentCsvFilePath;
+    protected long writerTimeout = 60000;
 
-    protected volatile CsvWriter csvWriter;
+    protected CsvDataWriter csvDataWriter;
+
+    private Object timestampKey;
 
     public CsvStore() {
         csvConfig = new CsvConfig();
@@ -60,6 +65,9 @@ public class CsvStore implements DataStore {
         csvConfig.setIgnoreEmptyLines(true);
         csvConfig.setIgnoreLinePatterns(Pattern.compile("^#.*$"));
         csvConfig.setSkipLines(1);
+
+        csvDataWriter = new CsvDataWriter();
+        csvDataWriter.start();
     }
 
     public CsvStore(final CsvConfig csvConfig) {
@@ -72,73 +80,29 @@ public class CsvStore implements DataStore {
             throw new EmprosDataStoreException("WEMC0001");
         }
 
-        final CsvWriter writer = getCsvWriter();
-
         for (final Event event : eventList) {
             final List<String> valueList = new ArrayList<String>(
                     columnList.size());
             for (final String key : columnList) {
                 final Object value = event.get(key);
                 valueList.add(value != null ? value.toString() : null);
-            }
-            try {
-                synchronized (writer) {
-                    writer.writeValues(valueList);
+
+                final CsvData csvData = new CsvData();
+                final Object timestamp = event.get(timestampKey);
+                if (timestamp instanceof Long) {
+                    csvData.timestamp = ((Long) timestamp).longValue();
+                } else {
+                    csvData.timestamp = System.currentTimeMillis();
                 }
-            } catch (final Exception e) {
-                throw new EmprosDataStoreException("EEMC0004",
-                        new Object[] { event }, e);
+                csvData.valueList = valueList;
+                csvDataWriter.add(csvData);
             }
         }
-
+        csvDataWriter.flush();
     }
 
     public void destroy() {
-        closeCsvWriter();
-    }
-
-    protected CsvWriter getCsvWriter() {
-        if (rotationDatePattern != null) {
-            final SimpleDateFormat sdf = new SimpleDateFormat(
-                    rotationDatePattern);
-            final String baseName = sdf.format(new Date());
-            return createCsvWriter(baseName);
-        } else if (csvWriter != null) {
-            return csvWriter;
-        }
-        return createCsvWriter("data");
-    }
-
-    protected synchronized CsvWriter createCsvWriter(final String baseName) {
-        final File csvFile = new File(csvDirectoryPath, csvNamePrefix
-                + baseName + csvNameSuffix);
-        final String csvFilePath = csvFile.getAbsolutePath();
-        if (csvFilePath.equals(currentCsvFilePath)) {
-            return csvWriter;
-        }
-        if (csvWriter != null) {
-            closeCsvWriter();
-        }
-        try {
-            csvWriter = new CsvWriter(new BufferedWriter(
-                    new OutputStreamWriter(new FileOutputStream(csvFile),
-                            outputEncoding)), csvConfig);
-            currentCsvFilePath = csvFilePath;
-            logger.info("Current CSV File: {}", csvFilePath);
-        } catch (final Exception e) {
-            throw new EmprosDataStoreException("EEMC0003",
-                    new Object[] { csvFilePath }, e);
-        }
-        return csvWriter;
-    }
-
-    private void closeCsvWriter() {
-        try {
-            csvWriter.flush();
-        } catch (final IOException e) {
-            logger.warn("Failed to flush a csv writer.", e);
-        }
-        IOUtils.closeQuietly(csvWriter);
+        csvDataWriter.destroy();
     }
 
     public List<String> getColumnList() {
@@ -187,6 +151,132 @@ public class CsvStore implements DataStore {
 
     public void setOutputEncoding(final String outputEncoding) {
         this.outputEncoding = outputEncoding;
+    }
+
+    public long getWriterTimeout() {
+        return writerTimeout;
+    }
+
+    public void setWriterTimeout(final long writerTimeout) {
+        this.writerTimeout = writerTimeout;
+    }
+
+    protected static class CsvData {
+        protected long timestamp;
+
+        protected List<String> valueList;
+
+        @Override
+        public String toString() {
+            return "CsvData [timestamp=" + timestamp + ", valueList="
+                    + valueList + "]";
+        }
+    }
+
+    protected class CsvDataWriter extends Thread {
+        protected Queue<CsvData> queue = new ConcurrentLinkedQueue<CsvData>();
+
+        protected CsvWriter csvWriter;
+
+        protected String currentCsvFilePath;
+
+        protected boolean running;
+
+        public void add(final CsvData csvData) {
+            queue.add(csvData);
+        }
+
+        public void flush() {
+            synchronized (CsvStore.this) {
+                CsvStore.this.notify();
+            }
+        }
+
+        @Override
+        public void destroy() {
+            running = false;
+            interrupt();
+        }
+
+        @Override
+        public void run() {
+            running = true;
+            boolean alive = true;
+            while (alive) {
+                if (queue.isEmpty()) {
+                    if (running) {
+                        synchronized (CsvStore.this) {
+                            try {
+                                CsvStore.this.wait(writerTimeout);
+                            } catch (final InterruptedException e) {
+                            }
+                        }
+                        if (queue.isEmpty()) {
+                            closeCsvWriter();
+                        }
+                    } else {
+                        alive = false;
+                    }
+                } else {
+                    final CsvData csvData = queue.poll();
+                    try {
+                        final CsvWriter writer = getCsvWriter(csvData.timestamp);
+                        writer.writeValues(csvData.valueList);
+                    } catch (final Exception e) {
+                        logger.error(MessageFormatter.getSimpleMessage(
+                                "EEMC0004", new Object[] { csvData }), e);
+                    }
+                }
+            }
+            closeCsvWriter();
+        }
+
+        protected CsvWriter getCsvWriter(final long timestamp) {
+            if (rotationDatePattern != null) {
+                final SimpleDateFormat sdf = new SimpleDateFormat(
+                        rotationDatePattern);
+                final String baseName = sdf.format(new Date(timestamp));
+                return createCsvWriter(baseName);
+            } else if (csvWriter != null) {
+                return csvWriter;
+            }
+            return createCsvWriter("data");
+        }
+
+        protected CsvWriter createCsvWriter(final String baseName) {
+            final File csvFile = new File(csvDirectoryPath, csvNamePrefix
+                    + baseName + csvNameSuffix);
+            final String csvFilePath = csvFile.getAbsolutePath();
+            if (csvWriter != null) {
+                if (csvFilePath.equals(currentCsvFilePath)) {
+                    return csvWriter;
+                }
+                closeCsvWriter();
+            }
+            try {
+                csvWriter = new CsvWriter(new BufferedWriter(
+                        new OutputStreamWriter(new FileOutputStream(csvFile),
+                                outputEncoding)), csvConfig);
+                currentCsvFilePath = csvFilePath;
+                logger.info("Current CSV File: {}", csvFilePath);
+            } catch (final Exception e) {
+                throw new EmprosDataStoreException("EEMC0003",
+                        new Object[] { csvFilePath }, e);
+            }
+            return csvWriter;
+        }
+
+        protected void closeCsvWriter() {
+            if (csvWriter != null) {
+                try {
+                    csvWriter.flush();
+                } catch (final IOException e) {
+                    logger.warn("Failed to flush a csv writer.", e);
+                }
+                IOUtils.closeQuietly(csvWriter);
+                csvWriter = null;
+            }
+        }
     }
 
 }
